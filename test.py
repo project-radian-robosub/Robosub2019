180cdbffce0f57e97aa45a024d4c0098b678f03c
justin5901 <47486763+justin5901@users.noreply.github.com>
2019-07-20 11:00:29 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import board
+import busio
+import adafruit_bno055
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055(i2c, address=0x28)
+
+print('Temperature: {} degrees C'.format(sensor.temperature))
+print('Accelerometer (m/s^2): {}'.format(sensor.accelerometer))
+print('Magnetometer (microteslas): {}'.format(sensor.magnetometer))
+print('Gyroscope (deg/sec): {}'.format(sensor.gyroscope))
+print('Euler angle: {}'.format(sensor.euler))
+print('Quaternion: {}'.format(sensor.quaternion))
+print('Linear acceleration (m/s^2): {}'.format(sensor.linear_acceleration))
+print('Gravity (m/s^2): {}'.format(sensor.gravity))
+print(i2c.scan())
+
+
+while True:
+
+    print(sensor.euler)

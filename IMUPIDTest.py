c2881491127f4fbe17844964a61daf2ab2294ab2
justin5901 <47486763+justin5901@users.noreply.github.com>
2019-07-23 15:37:12 -0700

    Add files via upload

--- IMUPIDTest.py
+++ IMUPIDTest.py
@@ -1,12 +1,21 @@
 import IMU
 import MotorMovement
 
-imu = IMU.IMU(1, 0, 0, 0, 0, 0)
+imu = IMU.IMU(0.5, 0, 0, 0, 0, 0)
 
 motor2 = MotorMovement.motor_coroutine(0)
 motor7 = MotorMovement.motor_coroutine(5)
 motor2_power = 0
 motor7_power = 0
+
+
+def wait_for_arduino():
+    msg = ""
+    while msg.find("ready") == -1:
+        if ser.inWaiting() > 0:
+            c = ser.read()
+            msg += c.decode('utf-8')
+            print("Arduino" + msg)
 
 
 def remap(x, b1, b2, v1, v2):
@@ -22,11 +31,12 @@ def stop_all():
 
 
 try:
+    wait_for_arduino()
     motor2.send(0)
     motor7.send(0)
     while True:
         motor2_power = remap(imu.get_pid()[2], -180, 180, -100, 100)
-        motor7_power = remap(imu.get_pid()[2], -180, 180, -100, 100) * -1
+        motor7_power = -remap(imu.get_pid()[2], -180, 180, -100, 100)
         motor2.send(int(motor2_power))
         motor7.send(int(motor7_power))
 

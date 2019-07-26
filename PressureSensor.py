db1d2c52961dc46d767dfc53c56cd1a73311e477
justin5901 <justin5901@gmail.com>
2019-07-25 22:20:45 -0700

    Fixed IMU Math

--- PressureSensor.py
+++ PressureSensor.py
@@ -8,11 +8,9 @@ class Pressure:
     kp = 0
     ki = 0
     kd = 0
-    setpoint = 0
 
     def __init__(self, kp, ki, kd, setpoint=0):
         self.pid_pressure = PID(kp, ki, kd, setpoint, output_limits=(-60, 60))
-        self.setpoint = setpoint
         self.kp = kp
         self.ki = ki
         self.kd = kd
@@ -30,5 +28,4 @@ class Pressure:
         return pid_val
 
     def set_tar(self, val):
-        self.setpoint = val
-        self.pid_pressure = PID(self.kp, self.ki, self.kd, self.setpoint)
+        self.pid_pressure.setpoint = val

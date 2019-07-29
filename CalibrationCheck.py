1664e032135a5da84d31a8e4b1d8627cf73366ba
justin5901 <justin5901@gmail.com>
2019-07-29 12:53:35 -0700

    calibration check

---
+++ CalibrationCheck.py
@@ -0,0 +1,12 @@
+import time
+
+import adafruit_bno055
+import board
+import busio
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055(i2c)
+
+while True:
+    print(sensor.getCalibration())
+    time.sleep(.1)

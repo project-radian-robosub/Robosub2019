6f194da07256482583aae606bfff087789105f6d
justin5901 <justin5901@gmail.com>
2019-07-30 12:07:02 -0700

    qualification?

---
+++ QualificationDeadReckon.py
@@ -0,0 +1,52 @@
+import time
+
+import Control
+
+ctr = Control
+
+ctr.MotorMovement.wait_for_arduino()
+
+ctr.stop_all()
+
+time.sleep(0.1)
+
+try:
+    # ctr.imu.set_z(50)
+    ctr.pressure.set_tar(1100)
+    while ctr.pressure.get_val() < 1080:
+        ctr.set_imu_powers()
+        ctr.set_pressure_powers()
+        ctr.set_move_powers(0, 0, 0, 0, 0, 0)
+        ctr.set_motor_powers()
+        print(ctr.imu.get_angles(), ctr.imu.get_angle_pid(), ctr.imu.center_z)
+
+    timer1 = time.perf_counter()
+    timer2 = time.perf_counter()
+
+    while timer2 - timer1 < 30:  # forward
+        ctr.set_imu_powers()
+        ctr.set_pressure_powers()
+        ctr.set_move_powers(75, 0, 0, 0, 0, 75)
+        ctr.set_motor_powers()
+        timer2 = time.perf_counter()
+        print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
+
+    timer1 = time.perf_counter()
+    timer2 = time.perf_counter()
+
+    while timer2 - timer1 < 4:  # stop
+        ctr.set_imu_powers()
+        ctr.set_pressure_powers()
+        ctr.set_move_powers(0, 0, 0, 0, 0, 0)
+        ctr.set_motor_powers()
+        timer2 = time.perf_counter()
+        print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
+
+
+except KeyboardInterrupt:
+    ctr.stop_all()
+    print("end")
+
+finally:
+    ctr.stop_all()
+    print("end")

71267270b65fd41e36a3b7af233d0067c89c9b8d
justin5901 <justin5901@gmail.com>
2019-07-30 22:53:39 -0700

    reboot

--- DiveAndMaintain.py
+++ DiveAndMaintain.py
@@ -4,28 +4,36 @@ import Control
 
 ctr = Control
 
-ctr.MotorMovement.wait_for_arduino()
+killed = True
 
-ctr.stop_all()
+while killed:
 
-time.sleep(0.1)
+    ctr.MotorMovement.wait_for_arduino()
 
-try:
-    # ctr.imu.set_z(50)
-    ctr.pressure.set_tar(1070)
+    killed = False
 
-    while True:
-        ctr.set_imu_powers()
-        # ctr.set_pressure_powers()
-        ctr.set_move_powers(0, 0, 0, 0, 0, 0)
-        ctr.set_motor_powers()
-        print(ctr.imu.get_angles(), ctr.imu.get_angle_pid(), ctr.imu.center_z)
-        timer2 = time.perf_counter()
-
-except KeyboardInterrupt:
     ctr.stop_all()
-    print("end")
 
-finally:
-    ctr.stop_all()
-    print("end")
+    time.sleep(0.1)
+
+    try:
+        # ctr.imu.set_z(50)
+        ctr.pressure.set_tar(1070)
+
+        while not killed:
+            ctr.set_imu_powers()
+            # ctr.set_pressure_powers()
+            ctr.set_move_powers(0, 0, 0, 0, 0, 0)
+            ctr.set_motor_powers()
+            print(ctr.imu.get_angles(), ctr.imu.get_angle_pid(), ctr.imu.center_z)
+            timer2 = time.perf_counter()
+            if ctr.MotorMovement.check_reset():
+                killed = True
+
+    except KeyboardInterrupt:
+        ctr.stop_all()
+        print("end")
+
+    finally:
+        ctr.stop_all()
+        print("end")

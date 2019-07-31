ee596c4b19bdbd23784052e095f4c83dd1dd7c09
justin5901 <justin5901@gmail.com>
2019-07-31 10:06:47 -0700

    Refine files

--- Tests/SendSerial.py
+++
@@ -1,8 +0,0 @@
-from serial import Serial
-
-ser = Serial("/dev/ttyACM0", 9600)  # change the serial port to whatever the arduino is connected to
-
-
-while True:
-    string = (input("values?" ))
-    ser.write(string.encode())
--- Tests/timetest.py
+++
@@ -1,7 +0,0 @@
-import time
-
-import IMU
-
-while True:
-    print(IMU.sensor.euler, IMU.sensor.acceleration)
-    time.sleep(0.1)

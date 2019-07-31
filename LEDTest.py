fb6fa5b340526e93a34e9a7ebe79044edeaeed77
justin5901 <justin5901@gmail.com>
2019-07-30 21:06:55 -0700

    LED

--- LEDTest.py
+++ LEDTest.py
@@ -5,10 +5,5 @@ import Jetson.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(21, GPIO.OUT)
 time.sleep(1)
-GPIO.output(21, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(21, GPIO.LOW)
-time.sleep(1)
-GPIO.output(21, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(21, GPIO.LOW)
+while True:
+    GPIO.output(21, GPIO.HIGH)

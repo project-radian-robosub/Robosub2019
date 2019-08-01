a3c5e1818f214270983d8f0900b94c652b4e7a26
justin5901 <justin5901@gmail.com>
2019-08-01 10:56:34 -0700

    sensor test

--- sensortest.py
+++ sensortest.py
@@ -2,11 +2,14 @@
 import time
 
 import IMU
+import MotorMovement
 from PressureSensor import Pressure
 
 p = Pressure()
 
 while True:
+    if MotorMovement.imu.sensor.calibration_status[3] > 2:
+        MotorMovement.GPIO.output(40, MotorMovement.GPIO.HIGH)
     IMU.sensor.mode = IMU.sensor.NDOF_FMC_OFF_MODE
     print(p.get_val(), IMU.sensor.magnetic, IMU.sensor.euler)
     time.sleep(0.1)

1aa3da3e2cb5241c9599d7042e2f127b32bce472
justin5901 <justin5901@gmail.com>
2019-08-01 10:55:19 -0700

    sensor test

--- sensortest.py
+++ sensortest.py
@@ -7,6 +7,6 @@ from PressureSensor import Pressure
 p = Pressure()
 
 while True:
-
+    IMU.sensor.mode = IMU.sensor.NDOF_FMC_OFF_MODE
     print(p.get_val(), IMU.sensor.magnetic, IMU.sensor.euler)
     time.sleep(0.1)

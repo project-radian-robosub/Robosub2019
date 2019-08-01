338bc70a9481071503746a516933f68a8ad9a226
stewe951 <stewe951@gmail.com>
2019-08-01 00:46:19 -0700

    fixed boxes on VisionV3`

--- Vision/BackgroundSubtractor.py
+++ Vision/BackgroundSubtractor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import time
 
 
 class VisionV3:
@@ -7,12 +8,15 @@ class VisionV3:
     def __init__(self, cam_number):
         self.cap = cv2.VideoCapture(cam_number)
 
+        self.width = 640
+        self.height = 480
+
         self.cap.set(cv2.CAP_PROP_FPS, 15.0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 
     def vision_generator(self, show=False):
-        boxes = ['null']
+        boxes = [None]
 
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
         fgbg = cv2.createBackgroundSubtractorMOG2()
@@ -21,38 +25,55 @@ class VisionV3:
             success, img = self.get_cap().read()
             # print('justin')
             if success:
-                height, width, _ = img.shape
-                min_x, min_y = width, height
-                max_x = max_y = 0
-
                 blur = cv2.GaussianBlur(img, (5, 5), 10)
                 fgmask = fgbg.apply(blur)
                 fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
                 contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                height, width, _ = img.shape
+                min_x, min_y = width, height
+                max_x = max_y = 0
 
                 for i in range(len(contours)):
-                    if len(contours) > 0:
-                        x, y, w, h = cv2.boundingRect(contours[i])
 
-                        # tl = (rect_x, rect_y)
-                        # br = (rect_x+rect_w, rect_y+rect_h)
+
+                    x, y, w, h = cv2.boundingRect(contours[i])
+
+                    if w > 25 and h > 80:
+                        '''
+                        tl = (x, y)
+                        br = (x + w, y + h)
+                        '''
 
                         min_x, max_x = min(x, min_x), max(x + w, max_x)
                         min_y, max_y = min(y, min_y), max(y + h, max_y)
 
-                        if w > 25 and h > 80:
-                            rect = cv2.minAreaRect(contours[i])
-                            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                            box = cv2.boxPoints(rect)
-                            box_d = np.int0(box)
-                            cv2.drawContours(img, [box_d], 0, (0, 255, 0), 3)
-                            # cv2.drawContours(blur, contours[i], -1, (255, 0, 0))
+                        tl = (min_x, min_y)
+                        br = (max_x, max_y)
+
+                        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                        cv2.rectangle(img, (min_x, min_y), (max_x, max_y), (0, 255, 0), 3)
+
+                        box_ori = (tl, br)
+                        # boxes.append(box_ori)
+
+                        # rect = cv2.minAreaRect(contours[i])
+                        # box = cv2.boxPoints(rect)
+                        # box_d = np.int0(box)
+                        # cv2.drawContours(img, [box_d], 0, (0, 255, 0), 3)
+                        # cv2.drawContours(blur, contours[i], -1, (255, 0, 0))
                 if show:
-                    cv2.imshow('Image', blur)
+                    w, q = tuple(box_ori)
+                    x1, y1 = w
+                    x2, y2 = q
+
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    print(box_ori)
+
+                    cv2.imshow('Image', img)
                     cv2.imshow('contour', fgmask)
-                print(boxes)
-                yield boxes
+
+                yield box_ori
             yield
 
     def get_cap(self):
@@ -60,6 +81,12 @@ class VisionV3:
 
     def set_cap(self, cam_num):
         self.cap = cam_num
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
 
 
 if __name__ == '__main__':

221b92cb908bfe23437274c7fc15375cc69cec2c
Justin Rhee <justin5901@gmail.com>
2019-08-01 14:07:10 -0700

    asdf

--- DriveBox.py
+++ DriveBox.py
@@ -4,7 +4,7 @@ from Vision.BackgroundSubtractor import VisionV3
 
 import Control
 
-v1 = VisionV3(0)
+v1 = VisionV3(1)
 gen = v1.vision_generator(True)
 ctr = Control
 killed = True
@@ -20,8 +20,9 @@ while killed:
     time.sleep(0.1)
 
     try:
-        ctr.pressure.set_tar(1100)
-        while ctr.pressure.get_val() < 1080 and not killed:
+
+        ctr.pressure.set_tar(1070)
+        while ctr.pressure.get_val() < 1060 and not killed:
             ctr.set_imu_powers()
             ctr.set_pressure_powers()
             ctr.set_move_powers(0, 0, 0, 0, 0, 0)
@@ -35,19 +36,18 @@ while killed:
         timer2 = time.perf_counter()
 
         while gen.__next__() is None and not killed:  # forward
-            print(gen.__next__())
             ctr.set_imu_powers()
             ctr.set_pressure_powers()
             ctr.set_move_powers(75, 0, 0, 0, 0, 75)
             ctr.set_motor_powers()
             timer2 = time.perf_counter()
-            print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
+            # print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
             if ctr.MotorMovement.check_reset():
                 killed = True
                 print('KILLED')
 
         while not gen.__next__() is None and not killed:  # forward
-            print(gen.__next__())
+            gen.__next__()
             tl, br = gen.__next__()
             x = (tl[0] + br[0]) / 2
             y = (tl[1] + br[1]) / 2
@@ -66,7 +66,7 @@ while killed:
             ctr.set_move_powers(75, x_pow, -y_pow, -y_pow, x_pow, 75)
             ctr.set_motor_powers()
             timer2 = time.perf_counter()
-            print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
+            # print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
             if ctr.MotorMovement.check_reset():
                 killed = True
                 print('KILLED')
@@ -80,7 +80,7 @@ while killed:
             ctr.set_move_powers(0, 0, 0, 0, 0, 0)
             ctr.set_motor_powers()
             timer2 = time.perf_counter()
-            print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
+            # print(ctr.imu.get_angles(), ctr.pressure.get_val(), ctr.MotorMovement.targets)
             if ctr.MotorMovement.check_reset():
                 killed = True
                 print('KILLED')

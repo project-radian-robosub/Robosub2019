f24d7962e92fb5a033f0721d126b713f7070d8a1
justin5901 <justin5901@gmail.com>
2019-08-01 16:57:08 -0700

    fixed?

--- sensortest.py
+++ sensortest.py
@@ -1,16 +1,15 @@
 
 import time
-import adafruit_bno055
+
 import IMU
 import MotorMovement
-from PressureSensor import Pressure
 
-p = Pressure()
+# from PressureSensor import Pressure
+
+# p = Pressure()
 
 while True:
     if MotorMovement.imu.sensor.calibration_status[3] > 2:
         MotorMovement.GPIO.output(40, MotorMovement.GPIO.HIGH)
-    # IMU.sensor.mode = adafruit_bno055.ACCGYRO_MODE
-    # print(IMU.sensor.mode)
-    print(p.get_val(), IMU.sensor.euler)
+    print(IMU.sensor.euler)
     time.sleep(0.1)

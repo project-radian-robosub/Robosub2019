1fefa05c00dc23ecd271a6d0ffea2725113649de
Justin Rhee <justin5901@gmail.com>
2019-08-02 11:57:19 -0700

    troubleshooting

--- sensortest.py
+++ sensortest.py
@@ -3,12 +3,12 @@ import time
 
 import IMU
 import MotorMovement
-# from PressureSensor import Pressure
+from PressureSensor import Pressure
 
-# p = Pressure()
+p = Pressure()
 
 while True:
     if MotorMovement.imu.sensor.calibration_status[3] > 2:
         MotorMovement.GPIO.output(40, MotorMovement.GPIO.HIGH)
-    print(IMU.sensor.euler)
+    print(p.get_val(), IMU.sensor.euler)
     time.sleep(0.1)
